7bb2b81ddfd1fb10631a8296f0cb96971776940e
vietnguyen97 <tvietnguyen1997@gmail.com>
2025-03-19 20:54:35 +0700

    chore(release): publish 2.0.0

---
+++ app1/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'app1',
+  preset: '../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/app1',
+};
---
+++ shared-utils/src/lib/shared-utils.tsx
@@ -0,0 +1,9 @@
+export function SharedUtils() {
+  return (
+    <div>
+      <h1>Welcome to SharedUtils!</h1>
+    </div>
+  );
+}
+
+export default SharedUtils;
