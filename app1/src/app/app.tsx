bb06a1ca5cb70ecfcc176a7f411e1ca38dff389b
vietnguyen97 <tvietnguyen1997@gmail.com>
2025-03-19 22:31:29 +0700

    fix(app1): fix content
    https://www.google.com/

--- app1/src/app/app.tsx
+++ app1/src/app/app.tsx
@@ -16,7 +16,7 @@ export function App() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/page-2">Page 2</Link>
+            <Link to="/page-2">Page 1</Link>
           </li>
         </ul>
       </div>
