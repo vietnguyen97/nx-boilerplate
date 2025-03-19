958c7a03b97ed3c9278f27fdf0e698cc8a4a57f4
vietnguyen97 <tvietnguyen1997@gmail.com>
2025-03-19 22:12:08 +0700

    fix(app2): comment

--- app2/src/app/app.tsx
+++ app2/src/app/app.tsx
@@ -7,9 +7,6 @@ export function App() {
     <div>
       <NxWelcome title="app2" />
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
       <br />
       <hr />
       <br />
